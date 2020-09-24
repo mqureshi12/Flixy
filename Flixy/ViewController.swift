c05f1605722406e80db0974678169207463a94ff
Mohammad <mqure016@fiu.edu>
2020-09-24 11:26:18 -0400

    initial commit

---
+++ Flixy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flixy
+//
+//  Created by Mohammad Qureshi on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
